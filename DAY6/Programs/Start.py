ecc559d5b78094ec84c2ddcbe99612a28fea1300
Abhilashkumar0079 <abhilashkumar213@gmail.com>
2024-05-03 20:25:35 +0530

    Added code practice of lecture.

---
+++ DAY6/Programs/Start.py
@@ -0,0 +1,32 @@
+#######  Built-in Function  #######
+# print()
+# input()
+# len()
+# type()
+# range()
+# round()
+# sum()
+# sorted()
+# list()
+# tuple()
+# dict()
+# set()
+# str()
+# int()
+# float()
+# bool()
+# max()
+# min()
+# any()
+# all()
+# enumerate()
+# reversed()
+######## User defined Function ########
+
+##define the functiion
+def my_function():
+    print("Hello")
+    print("Bye")
+
+### call the function
+my_function()
