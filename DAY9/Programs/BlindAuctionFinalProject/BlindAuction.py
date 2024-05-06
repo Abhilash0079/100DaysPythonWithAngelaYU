e7d1bd82e05c2549f74deb209fc1568adea0312e
ABHILASH KUMAR <abhilashkumar213@gmail.com>
2024-05-06 21:32:18 +0530

    Modify the condition

--- DAY9/Programs/BlindAuctionFinalProject/BlindAuction.py
+++ DAY9/Programs/BlindAuctionFinalProject/BlindAuction.py
@@ -21,10 +21,12 @@ while not bidding_finished:
     name = input("What is your name? ")
     price = int(input("What is your bid? $"))
     bids[name] = price
-    should_continue = input("Are there any more bidders? Type 'yes' or 'no'.\n ")
+    should_continue = input("Are there any more bidders? Type 'yes' or 'no'.\n ").lower()
     if should_continue == 'no':
         bidding_finished = True
         find_highest_bidder(bids)
     elif should_continue == "yes":
         os.system('cls')
+    else:
+        print("Invalid input. Please enter 'yes' or 'no'.")
 
