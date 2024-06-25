832ccddf8968ef91f799e8dd58c8e266dbb2328d
Abhilashkumar0079 <abhilashkumar213@gmail.com>
2024-06-25 23:10:51 +0530

    Save the highest score in file using file methods.

--- DAY21-SnakeGame-Part2/Programs/scoreboard.py
+++ DAY21-SnakeGame-Part2/Programs/scoreboard.py
@@ -9,7 +9,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open('D:/UDEMY/Python/100DaysPythonWithAngelaYU/DAY21-SnakeGame-Part2/Programs/data.txt') as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 268)
@@ -23,6 +24,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open('D:/UDEMY/Python/100DaysPythonWithAngelaYU/DAY21-SnakeGame-Part2/Programs/data.txt', mode='w') as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
 
