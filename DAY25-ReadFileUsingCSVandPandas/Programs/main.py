cf67d8d76154792ba297ca736202d72b4f01da0a
Abhilashkumar0079 <abhilashkumar213@gmail.com>
2024-06-26 21:47:11 +0530

    Read CSV file using pandas.

--- DAY25-ReadFileUsingCSVandPandas/Programs/main.py
+++ DAY25-ReadFileUsingCSVandPandas/Programs/main.py
@@ -1,20 +1,24 @@
-with open('D:/UDEMY/Python/100DaysPythonWithAngelaYU/DAY25-ReadFileUsingCSVandPandas/Resources/weather_data.csv') as data_file:
-    data = data_file.readlines()
-    print(data)
+# with open('D:/UDEMY/Python/100DaysPythonWithAngelaYU/DAY25-ReadFileUsingCSVandPandas/Resources/weather_data.csv') as data_file:
+#     data = data_file.readlines()
+#     print(data)
 
 
-import csv
+# import csv
 
-with open('D:/UDEMY/Python/100DaysPythonWithAngelaYU/DAY25-ReadFileUsingCSVandPandas/Resources/weather_data.csv') as data_file:
-    data = csv.reader(data_file)
-    # it will return an object "<_csv.reader object at 0x000001C82A2F0BE0>"
-    # print(data)
-    temperature = []
-    for row in data:
-        print(row)
-        if row[1] != 'temp':
-            temperature.append(int(row[1]))
+# with open('D:/UDEMY/Python/100DaysPythonWithAngelaYU/DAY25-ReadFileUsingCSVandPandas/Resources/weather_data.csv') as data_file:
+#     data = csv.reader(data_file)
+#     # it will return an object "<_csv.reader object at 0x000001C82A2F0BE0>"
+#     # print(data)
+#     temperature = []
+#     for row in data:
+#         print(row)
+#         if row[1] != 'temp':
+#             temperature.append(int(row[1]))
 
-    print(temperature)
+#     print(temperature)
 
+import pandas
 
+data = pandas.read_csv('D:/UDEMY/Python/100DaysPythonWithAngelaYU/DAY25-ReadFileUsingCSVandPandas/Resources/weather_data.csv')
+print(data)
+print(data['temp'])
