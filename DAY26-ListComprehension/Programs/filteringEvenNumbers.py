338ae79d3d95993a608815a2d700a7e1f3dbb066
Abhilashkumar0079 <abhilashkumar213@gmail.com>
2024-06-29 16:44:30 +0530

    DAY 26 filter even number using list comprehension.

---
+++ DAY26-ListComprehension/Programs/filteringEvenNumbers.py
@@ -0,0 +1,13 @@
+list_of_strings = input().split(',')
+# 🚨 Do  not change the code above
+
+# TODO: Use list comprehension to convert the strings to integers 👇:
+
+int_num = [int(num) for num in list_of_strings]
+# TODO: Use list comprehension to filter out the odd numbers
+# and store the even numbers in a list called "result"
+
+result = [num for num in int_num if num % 2 == 0]
+
+# Write your code 👆 above:
+print(result)
