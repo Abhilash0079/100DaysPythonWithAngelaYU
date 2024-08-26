09bd930af9fbd1fd191c0446592457b68ac7393e
Abhilashkumar0079 <abhilashkumar213@gmail.com>
2024-08-26 12:21:43 +0530

    Tkinter Label, Button and textarea widgets.

--- DAY27-Tkinter/Programs/start.py
+++ DAY27-Tkinter/Programs/start.py
@@ -8,5 +8,25 @@ windows.minsize(width=500, height=300)
 my_label = tkinter.Label(text="I am a label.", font=("Arial", 20, "bold"))
 # pack() function is used to display the label on screen, by default in the middle.
 my_label.pack()
+# Change the property
+my_label.config(text='New Text')
+my_label['text'] = 'Abhilash'
+
+
+# Button
+
+def button_command():
+    print("I got clicked.")
+    my_label['text'] = "Clicked"
+    my_label.config(text=text_area.get())
+
+
+button = tkinter.Button(text="Click Me", command=button_command)
+button.pack()
+
+# Entry
+
+text_area = tkinter.Entry(width=10)
+text_area.pack()
 
 windows.mainloop()
