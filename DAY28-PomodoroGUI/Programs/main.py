e33871ed4c2eb2f64ad36e8eb0d83d811130dd02
Abhilashkumar0079 <abhilashkumar213@gmail.com>
2024-08-27 19:56:00 +0530

    Edit Image Path

--- DAY28-PomodoroGUI/Programs/main.py
+++ DAY28-PomodoroGUI/Programs/main.py
@@ -64,7 +64,7 @@ title_label = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 40, "bold"), bg=YEL
 title_label.grid(column=1, row=0)
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
-tomato_img = PhotoImage(file="tomato.png")
+tomato_img = PhotoImage(file="D:/UDEMY/Python/100DaysPythonWithAngelaYU/DAY28-PomodoroGUI/Resources/tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
