617213b30947199a7937b478c034cac0caff13d5
Abhilashkumar0079 <abhilashkumar213@gmail.com>
2024-08-28 08:51:29 +0530

    Edit Image Path.

--- DAY29-PasswordManagerGUI/Programs/main.py
+++ DAY29-PasswordManagerGUI/Programs/main.py
@@ -9,7 +9,7 @@ window.title("Password Generator")
 window.config(padx=20, pady=20)
 
 canvas = Canvas(width=200, height=200)
-pass_img = PhotoImage(file="logo.png")
+pass_img = PhotoImage(file="D:/UDEMY/Python/100DaysPythonWithAngelaYU/DAY29-PasswordManagerGUI/Resources/logo.png")
 canvas.create_image(100, 100, image=pass_img)
 canvas.grid(column=1, row=0)
 
