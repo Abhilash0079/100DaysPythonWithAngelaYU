54ec5dd29f2566e7e1326696fd7400904da69e06
Abhilashkumar0079 <abhilashkumar213@gmail.com>
2024-08-28 08:49:50 +0530

    Challenge 1. Setting the canvas and work with Image.

---
+++ DAY29-PasswordManagerGUI/Programs/main.py
@@ -0,0 +1,16 @@
+from tkinter import *
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Generator")
+window.config(padx=20, pady=20)
+
+canvas = Canvas(width=200, height=200)
+pass_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=pass_img)
+canvas.grid(column=1, row=0)
+
+window.mainloop()
