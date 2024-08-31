e188052c7eca86e7bae2754edf3899389bfb7765
Abhilashkumar0079 <abhilashkumar213@gmail.com>
2024-08-31 12:29:05 +0530

    Crearting New Flash Card.

--- DAY31-FlashCardCapstoneProject/Programs/main.py
+++ DAY31-FlashCardCapstoneProject/Programs/main.py
@@ -1,7 +1,21 @@
+import pandas
 from tkinter import *
+import random
 
 # ------------------------------ CONSTANT ------------------------- #
 BACKGROUND_COLOR = "#B1DDC6"
+data = pandas.read_csv("D:/UDEMY/Python/100DaysPythonWithAngelaYU/DAY31-FlashCardCapstoneProject/Resources/data/french_words.csv")
+to_learn = data.to_dict(orient="records")
+
+
+# ------------------------------ BUTTON FUNCTIONALITY ------------- #
+def next_card():
+    global current_card, flip_timer
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+
 
 # ------------------------------ USER INTERFACE ------------------- #
 window = Tk()
@@ -18,12 +32,13 @@ card_word = canvas.create_text(400, 263, text="Word", font=("Ariel", 60, "bold")
 canvas.grid(row=0, column=0, columnspan=2)
 
 cross_image = PhotoImage(file="D:/UDEMY/Python/100DaysPythonWithAngelaYU/DAY31-FlashCardCapstoneProject/Resources/images/wrong.png")
-unknown_button = Button(image=cross_image, highlightthickness=0)
+unknown_button = Button(image=cross_image, highlightthickness=0, command=next_card)
 unknown_button.grid(row=1, column=0)
 
 check_image = PhotoImage(file="D:/UDEMY/Python/100DaysPythonWithAngelaYU/DAY31-FlashCardCapstoneProject/Resources/images/right.png")
-known_button = Button(image=check_image, highlightthickness=0)
+known_button = Button(image=check_image, highlightthickness=0, command=next_card)
 known_button.grid(row=1, column=1)
 
+next_card()
 
 window.mainloop()
