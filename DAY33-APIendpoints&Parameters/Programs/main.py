3d2998fd1e8a8fafcdf0709d7c3fde131b792c59
Abhilashkumar0079 <abhilashkumar213@gmail.com>
2024-09-03 09:09:56 +0530

    Status message and Data from JSON.

--- DAY33-APIendpoints&Parameters/Programs/main.py
+++ DAY33-APIendpoints&Parameters/Programs/main.py
@@ -2,3 +2,16 @@ import requests
 
 response = requests.get("http://api.open-notify.org/iss-now.json")
 print(response)
+
+# Raise the exception
+response.raise_for_status()
+
+# JSON Data
+data = response.json()
+print(data)
+
+longitude = data["iss_position"]["longitude"]
+latitude = data["iss_position"]["latitude"]
+
+iss_position = (longitude, latitude)
+print(iss_position)
