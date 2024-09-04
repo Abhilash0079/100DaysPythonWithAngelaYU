2d6d6d4732c73d636e0c4cabd887b841c552dd09
Abhilashkumar0079 <abhilashkumar213@gmail.com>
2024-09-04 20:00:41 +0530

    Using the interface.

--- DAY34-APIpractice&QUIZAPP/Programs/data.py
+++ DAY34-APIpractice&QUIZAPP/Programs/data.py
@@ -9,7 +9,6 @@ response = requests.get("https://opentdb.com/api.php", params=parameters)
 response.raise_for_status()
 
 data = response.json()
-# print(data["results"])
 question_data = data["results"]
 
 # question_data = [
