a231f84eba80654a4c818ab3225f83d029796cee
Abhilashkumar0079 <abhilashkumar213@gmail.com>
2024-09-05 12:12:25 +0530

    Printing Rain Alert on console.

--- DAY35-OpenWeatherMapRainAlert/Programs/main.py
+++ DAY35-OpenWeatherMapRainAlert/Programs/main.py
@@ -8,10 +8,21 @@ parameters = {
     "lat": MY_LAT,
     "lon": MY_LONG,
     "appid": API_KEY,
+    "units": "metric",
+    "cnt": 4
 }
 
 response = requests.get("https://api.openweathermap.org/data/2.5/forecast", params=parameters)
 response.raise_for_status()
 
-data = response.json()
-print(data)
+weather_data = response.json()
+# print(weather_data["list"][0]["weather"][0]["id"])
+
+will_rain = False
+for hour_data in weather_data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    print("Bring an Umbrella.")
