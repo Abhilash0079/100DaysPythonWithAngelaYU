0d4b0dee41e63605c55b5046ea72ba8c08d326f1
Abhilashkumar0079 <abhilashkumar213@gmail.com>
2024-09-07 18:25:30 +0530

    Complete Stock News Alert using API of STOCK, NEWS and TWILIO for message.

--- DAY36-StockTradingNewsAlert/Programs/main.py
+++ DAY36-StockTradingNewsAlert/Programs/main.py
@@ -1,4 +1,6 @@
 import requests
+from twilio.rest import Client
+
 
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
@@ -7,6 +9,10 @@ STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 STOCK_API_KEY = "*******WAS****IQD*****"
+NEWS_API_KEY = "**d7154*****de6e***051*"
+
+TWILIO_SID = "******672e*****5f9fbe4***"
+TWILIO_TOKEN = "****302*****bf82**cdb9dc"
 # STEP 1: Use https://www.alphavantage.co/documentation/#daily
 # When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 
@@ -32,44 +38,44 @@ print(day_before_yesterday_closing_price)
 
 # Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20.
 
-difference = abs(yesterday_closing_price - day_before_yesterday_closing_price)
-print(difference)
+difference = yesterday_closing_price - day_before_yesterday_closing_price
+up_down = None
+if difference > 0:
+    up_down = "🔺"
+else:
+    up_down = "🔻"
 # Work out the percentage difference in price between closing price yesterday and closing price the
 #  day before yesterday.
-diff_percentage = (difference / yesterday_closing_price) * 100
+diff_percentage = round((difference / yesterday_closing_price) * 100)
 print(diff_percentage)
 # TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
-if diff_percentage > 5:
-    print("Get News.")
+# if diff_percentage > 5:
+#     print("Get News.")
 
 # STEP 2: https://newsapi.org/
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
 
 # TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
-
-# TODO 7. - Use Python slice operator to create a list that contains the first 3 articles.
-#  Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
-
+if abs(diff_percentage) > 5:
+    news_params = {
+        "apiKey": NEWS_API_KEY,
+        "qInTitle": COMPANY_NAME,
+    }
+    news_response = requests.get(NEWS_ENDPOINT, params=news_params)
+    articles = news_response.json()["articles"]
+    # Use Python slice operator to create a list that contains the first 3 articles.
+    three_articles = articles[:3]
+    print(three_articles)
 
 # STEP 3: Use twilio.com/docs/sms/quickstart/python
 # to send a separate message with each article's title and description to your phone number.
-
-# TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
-
-# TODO 9. - Send each article as a separate message via Twilio.
-
-
-# Optional TODO: Format the message like this:
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are 
-required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of 
-March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are 
-required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of 
-March 31st, near the height of the coronavirus market crash.
-"""
+    # Create a new list of the first 3 article's headline and description using list comprehension.
+    formatted_articles = [(f"{STOCK_NAME}: {up_down}{abs(diff_percentage)}%Headline: {article['title']}.\nBrief: {article['description']}") for article in three_articles]
+    # TODO 9. - Send each article as a separate message via Twilio.
+    client = Client(TWILIO_SID, TWILIO_TOKEN)
+    for article in formatted_articles:
+        message = client.messages.create(
+            body=article,
+            from_='+1***907301',
+            to='+917004605633'
+        )
