419112b9ccbed3dc323f4fb61d434dcaa75d8a61
Abhilashkumar0079 <abhilashkumar213@gmail.com>
2024-09-11 15:48:09 +0530

    Add a pixel in Habit Tracker.

--- DAY37-HabitTracker/Programs/main.py
+++ DAY37-HabitTracker/Programs/main.py
@@ -2,6 +2,7 @@ import requests
 
 TOKEN = "f*454***f3r**l"
 USERNAME = "abhilash1307"
+GRAPH_ID = "graph1"
 
 PIXELA_ENDPOINT = "https://pixe.la/v1/users"
 
@@ -21,7 +22,7 @@ user_params = {
 GRAPH_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs"
 
 graph_config = {
-    "id": "graph1",
+    "id": GRAPH_ID,
     "name": "Coding Practice",
     "unit": "commit",
     "type": "int",
@@ -32,5 +33,20 @@ headers = {
     "X-USER-TOKEN": TOKEN
 }
 
-response2 = requests.post(url=GRAPH_ENDPOINT, json=graph_config, headers=headers)
-print(response2.text)
+# response2 = requests.post(url=GRAPH_ENDPOINT, json=graph_config, headers=headers)
+# print(response2.text)
+
+# 3. Get the graph
+# https://pixe.la/v1/users/abhilash1307/graphs/graph1.html
+
+# 4. Post value on the graph
+
+PIXEL_CREATION_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs/{GRAPH_ID}"
+
+pixel_data = {
+    "date": "20240911",
+    "quantity": "5",
+}
+
+response3 = requests.post(url=PIXEL_CREATION_ENDPOINT, json=pixel_data, headers=headers)
+print(response3.text)
