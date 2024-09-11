2b1261c2714d902d4f78619384f959cfdcb3e5e3
Abhilashkumar0079 <abhilashkumar213@gmail.com>
2024-09-11 15:31:35 +0530

    Authentication using HTTP Header.

--- DAY37-HabitTracker/Programs/main.py
+++ DAY37-HabitTracker/Programs/main.py
@@ -1,13 +1,36 @@
 import requests
 
+TOKEN = "f*454***f3r**l"
+USERNAME = "abhilash1307"
+
 PIXELA_ENDPOINT = "https://pixe.la/v1/users"
 
 user_params = {
-    "token": "f*454***f3r**l",
-    "username": "abhilash1307",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
 
-response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
-print(response.text)
+# 1. Creating a user account on Pixela
+# response = requests.post(url=PIXELA_ENDPOINT, json=user_params)
+# print(response.text)
+
+# 2. Create a graph
+
+GRAPH_ENDPOINT = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "graph1",
+    "name": "Coding Practice",
+    "unit": "commit",
+    "type": "int",
+    "color": "shibafu",
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+response2 = requests.post(url=GRAPH_ENDPOINT, json=graph_config, headers=headers)
+print(response2.text)
