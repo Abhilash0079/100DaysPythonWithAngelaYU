fe80d53cfaec45ed6aa006cb95030ee239f1217c
Abhilashkumar0079 <abhilashkumar213@gmail.com>
2024-09-16 22:52:37 +0530

    Workout Tracking API request.

---
+++ DAY38-WorkoutTrackingUsingGoogleSheet/Programs/main.py
@@ -0,0 +1,30 @@
+import requests
+
+GENDER = "Male"
+WEIGHT_KG = "68"
+HEIGHT_CM = "170"
+AGE = "30"
+
+API_ID = "7007de72"
+API_KEY = "****55d7d****58bfd7****99b****"
+
+exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+exercise_text = input("Tell me which exercises you did: ")
+
+headers = {
+    "x-app-id": API_ID,
+    "x-app-key": API_KEY,
+}
+
+parameters = {
+    "query": exercise_text,
+    "gender": GENDER,
+    "weight_kg": WEIGHT_KG,
+    "height_cm": HEIGHT_CM,
+    "age": AGE
+}
+
+response = requests.post(exercise_endpoint, json=parameters, headers=headers)
+result = response.json()
+print(result)
