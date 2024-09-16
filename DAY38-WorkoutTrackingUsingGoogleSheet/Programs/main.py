d152fb9b67193f8d60af5be0e570764ce77ceeea
Abhilashkumar0079 <abhilashkumar213@gmail.com>
2024-09-17 00:27:15 +0530

    Save the data in google sheet.

--- DAY38-WorkoutTrackingUsingGoogleSheet/Programs/main.py
+++ DAY38-WorkoutTrackingUsingGoogleSheet/Programs/main.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 
 GENDER = "Male"
 WEIGHT_KG = "68"
@@ -9,6 +10,7 @@ API_ID = "7007de72"
 API_KEY = "****55d7d****58bfd7****99b****"
 
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+sheet_endpoint = "https://api.sheety.co/62d22509c8efb545f98d52dfa546587f/myWorkout/workouts"
 
 exercise_text = input("Tell me which exercises you did: ")
 
@@ -27,4 +29,23 @@ parameters = {
 
 response = requests.post(exercise_endpoint, json=parameters, headers=headers)
 result = response.json()
-print(result)
+
+# ################## Start of Step 4 Solution ##################### #
+
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
+
+for exercise in result["exercises"]:
+    sheet_inputs = {
+        "workout": {
+            "date": today_date,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+
+    sheet_response = requests.post(sheet_endpoint, json=sheet_inputs)
+
+    print(sheet_response.text)
